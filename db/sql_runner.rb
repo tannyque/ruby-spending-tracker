64893cce61299b2a9cefb9af8dcdb978f91c603d
Robert <robert.deignan@outlook.com>
2018-08-13 21:03:22 +0100

    Redo sqlrunner for heroku

--- db/sql_runner.rb
+++ db/sql_runner.rb
@@ -3,7 +3,14 @@ require 'pg'
 class SqlRunner
   def self.run(sql, values = [])
     begin
-      db = PG.connect(dbname: 'spending-tracker-4567')
+      if ENV['RACK_ENV'] == 'production'
+        db = PG.connect(
+          dbname: 'spending-tracker-4567',
+          host: ENV['DATABASE_URL']
+        )
+      else
+        db = PG.connect(dbname: 'spending-tracker-4567')
+      end
       db.prepare('query', sql)
       db.exec_prepared('query', values)
     ensure
